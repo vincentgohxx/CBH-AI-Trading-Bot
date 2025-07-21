d8ba76fa2ec486459fa3b2c047e04c08e1f6ed0d
vincentgohxx <52526227+vincentgohxx@users.noreply.github.com>
2025-07-21 16:20:57 +0800

    main.py

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import os
+import mimetypes
 from telegram.ext import Updater, CommandHandler, CallbackContext
-from telegram import Update
+from telegram import Update, InputFile
 
 def start(update: Update, context: CallbackContext):
     update.message.reply_text("Welcome to V-G Ai Trading Bot!")
+
+def is_image(file_path):
+    mime_type, _ = mimetypes.guess_type(file_path)
+    return mime_type and mime_type.startswith("image/")
 
 def main():
     token = os.getenv("BOT_TOKEN")
@@ -11,9 +16,8 @@ def main():
         print("Error: BOT_TOKEN not found in environment variables.")
         return
 
-    updater = Updater(token, use_context=True)
+    updater = Updater(token)
     dispatcher = updater.dispatcher
-
     dispatcher.add_handler(CommandHandler("start", start))
 
     updater.start_polling()
